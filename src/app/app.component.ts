2101b4bc0af96c12d772df8c84067ce2ca6e126a
Vanessa <vanessaquinoo@hotmail.com>
2025-01-14 19:49:30 -0300

    feat: criado o componente menu

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { NavComponent } from './nav/nav.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { AboutComponent } from './about/about.component';
+import { MenuComponent } from './menu/menu.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, CarouselComponent, AboutComponent],
+  imports: [NavComponent, CarouselComponent, AboutComponent, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
