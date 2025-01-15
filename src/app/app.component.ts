9d116a3f98d82b0c3ab3fe7152c98b3b7da9ab3b
Vanessa <vanessaquinoo@hotmail.com>
2025-01-15 17:18:39 -0300

    feat: criado e estilizado o component modal-local

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,10 +3,11 @@ import { NavComponent } from './nav/nav.component';
 import { CarouselComponent } from './carousel/carousel.component';
 import { AboutComponent } from './about/about.component';
 import { MenuComponent } from './menu/menu.component';
+import { ModalLocationComponent } from './modal-location/modal-location.component';
 
 @Component({
   selector: 'app-root',
-  imports: [NavComponent, CarouselComponent, AboutComponent, MenuComponent],
+  imports: [NavComponent, CarouselComponent, AboutComponent, MenuComponent, ModalLocationComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/modal-location/modal-location.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-modal-location',
+  imports: [],
+  templateUrl: './modal-location.component.html',
+  styleUrl: './modal-location.component.css'
+})
+export class ModalLocationComponent {
+
+}
